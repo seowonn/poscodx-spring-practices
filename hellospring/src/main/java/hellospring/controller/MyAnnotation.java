8ccf0c870ee0332580f16c2dff3de23f9e3b63ae
seowonn <sonseowon5@gmail.com>
2024-12-26 13:01:15 +0900

    Add: MVC response 반환 방법 실습

---
+++ hellospring/src/main/java/hellospring/controller/MyAnnotation.java
@@ -0,0 +1,16 @@
+package hellospring.controller;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(CLASS)
+@Target({ TYPE, FIELD, METHOD, PARAMETER })
+public @interface MyAnnotation {
+
+}
